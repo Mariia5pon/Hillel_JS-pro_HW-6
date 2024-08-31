41ed10b3c20058bf66c9ac5e70755fb1e317a5fa
mariia <mariia@mail.uni-paderborn.de>
2024-08-31 16:47:45 +0200

    completed task 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+// Task 1
+function removeCharacterArray() {
+    function removeCharacters(str, charsToRemove) {
+        let result = '';
+
+        for (let i = 0; i < str.length; i++) {
+            if (charsToRemove.indexOf(str[i]) === -1) {
+                result += str[i];
+            }
+        }
+
+        return result;
+    }
+
+    const inputString = prompt("Введіть рядок:");
+    const charsToRemoveString = prompt("Введіть символи для видалення:");
+
+    const charsToRemove = charsToRemoveString.split('');
+
+    const result = removeCharacters(inputString, charsToRemove);
+
+    document.getElementById('task-1_result').innerHTML = `Ваш рядок: ${inputString}. Символи для видалення: ${charsToRemoveString}. Результат: ${result}`;
+}
+
+// Task 2
 function calculateAverage(arr) {
     let sum = 0;
     let count = 0;
@@ -18,3 +43,5 @@ console.log(average);
 function logToConsole() {
     alert('Натисніть F12.');
 }
+
+//Task 3
